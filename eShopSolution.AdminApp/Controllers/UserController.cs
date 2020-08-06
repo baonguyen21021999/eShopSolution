76d2b1e0d9a72a13bed1f5674d5a05c0fd9f05c3
baonguyen21021999 <nvbao99x@gmail.com>
2020-08-06 08:33:36 +0700

    tim kiem

--- eShopSolution.AdminApp/Controllers/UserController.cs
+++ eShopSolution.AdminApp/Controllers/UserController.cs
@@ -28,7 +28,7 @@ namespace eShopSolution.AdminApp.Controllers
             _configuration = configuration;
         }
 
-        public async Task<IActionResult> Index(string keyword, int pageIndex = 1, int pageSize = 1)
+        public async Task<IActionResult> Index(string keyword, int pageIndex = 1, int pageSize = 10)
         {
             var request = new GetUserPagingRequest()
             {
@@ -37,6 +37,7 @@ namespace eShopSolution.AdminApp.Controllers
                 PageSize = pageSize
             };
             var data = await _userApiClient.GetUsersPagings(request);
+            ViewBag.Keyword = keyword;
             return View(data.ResultObj);
         }
         [HttpGet]
@@ -106,7 +107,7 @@ namespace eShopSolution.AdminApp.Controllers
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             HttpContext.Session.Remove("Token");
-            return RedirectToAction("Login", "User");
+            return RedirectToAction("Index", "Login");
         }
         [HttpGet]
         public IActionResult Delete(Guid id)
